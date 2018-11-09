ff22ac915fbea2af075831d45eae11a7f940c697
longzuowei <daydayup0318@163.com>
2018-11-09 08:33:37 +0800

    react目录文件

---
+++ src/containers/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "common/css/bootstrap.min.css";
+import "common/css/reset.css";
+import "common/css/common.css";
+import "common/css/style.css";
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div className="wraper">111</div>
+        )
+    }
+}
+
+export default App;
